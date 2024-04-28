1c3f1e8cde6ff7f6ead2e050c8276d2c46c31f5a
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-04-28 23:10:45 +0900

    add test code for checking Suffix checking

---
+++ src/test/java/io/github/hizumiaoba/mctimemachine/internal/SuffixCheckTest.java
@@ -0,0 +1,56 @@
+package io.github.hizumiaoba.mctimemachine.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+public class SuffixCheckTest {
+
+  @Test
+  public void testAlphaClient() {
+    // setup
+    VersionHelper.VersionObj client = VersionHelper.VersionObj.fromString("0.1.0-Alpha");
+
+    // tested
+    VersionHelper.VersionObj alpha = VersionHelper.VersionObj.fromString("0.1.0-Alpha");
+    VersionHelper.VersionObj beta = VersionHelper.VersionObj.fromString("0.1.0-Beta");
+    VersionHelper.VersionObj release = VersionHelper.VersionObj.fromString("0.1.0");
+
+    // verify
+    assertThat(client.checkSuffixStatus(alpha)).isTrue();
+    assertThat(client.checkSuffixStatus(beta)).isTrue();
+    assertThat(client.checkSuffixStatus(release)).isTrue();
+  }
+
+  @Test
+  public void testBetaClient() {
+    // setup
+    VersionHelper.VersionObj client = VersionHelper.VersionObj.fromString("0.1.0-Beta");
+
+    // tested
+    VersionHelper.VersionObj alpha = VersionHelper.VersionObj.fromString("0.1.0-Alpha");
+    VersionHelper.VersionObj beta = VersionHelper.VersionObj.fromString("0.1.0-Beta");
+    VersionHelper.VersionObj release = VersionHelper.VersionObj.fromString("0.1.0");
+
+    // verify
+    assertThat(client.checkSuffixStatus(alpha)).isFalse();
+    assertThat(client.checkSuffixStatus(beta)).isTrue();
+    assertThat(client.checkSuffixStatus(release)).isTrue();
+  }
+
+  @Test
+  public void testReleaseClient() {
+    // setup
+    VersionHelper.VersionObj client = VersionHelper.VersionObj.fromString("0.1.0");
+
+    // tested
+    VersionHelper.VersionObj alpha = VersionHelper.VersionObj.fromString("0.1.0-Alpha");
+    VersionHelper.VersionObj beta = VersionHelper.VersionObj.fromString("0.1.0-Beta");
+    VersionHelper.VersionObj release = VersionHelper.VersionObj.fromString("0.1.0");
+
+    // verify
+    assertThat(client.checkSuffixStatus(alpha)).isFalse();
+    assertThat(client.checkSuffixStatus(beta)).isFalse();
+    assertThat(client.checkSuffixStatus(release)).isTrue();
+  }
+}
