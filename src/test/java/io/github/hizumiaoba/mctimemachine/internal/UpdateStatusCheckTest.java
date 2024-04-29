ae3aaf42108778e441062f7841aa51e1dd0d865f
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-04-29 15:14:19 +0900

    add test for status checking

---
+++ src/test/java/io/github/hizumiaoba/mctimemachine/internal/UpdateStatusCheckTest.java
@@ -0,0 +1,24 @@
+package io.github.hizumiaoba.mctimemachine.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.github.hizumiaoba.mctimemachine.internal.VersionHelper.UpdateStatus;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class UpdateStatusCheckTest {
+
+  @Test
+  public void testStatusCheck() {
+    VersionHelper.VersionObj currentVersionMock = VersionHelper.VersionObj.fromString("1.0.0");
+
+    VersionHelper.VersionObj remoteVersionMock = VersionHelper.VersionObj.fromString("1.2.5-Beta");
+
+    Map<String, UpdateStatus> status = currentVersionMock.checkStatus(remoteVersionMock);
+
+    assertThat(status.get("major")).isEqualTo(UpdateStatus.UP_TO_DATE);
+    assertThat(status.get("minor")).isEqualTo(UpdateStatus.OUTDATED);
+    assertThat(status.get("patch")).isEqualTo(UpdateStatus.OUTDATED);
+    assertThat(status.get("suffix")).isEqualTo(UpdateStatus.UP_TO_DATE);
+  }
+}
