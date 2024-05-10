d9f445d4365e3e6549cc2cd18c4a71897a67810c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-05-10 14:54:27 +0900

    add experimental logic for checking update

---
+++ src/main/java/io/github/hizumiaoba/mctimemachine/internal/version/VersionHelper.java
@@ -0,0 +1,52 @@
+package io.github.hizumiaoba.mctimemachine.internal.version;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class VersionHelper {
+
+  private final VersionObj clientVersion;
+
+  public boolean checkLatest(VersionObj remoteVersion, boolean ignoreSuffix) {
+    return getUpdateType(remoteVersion, ignoreSuffix).equals("latest");
+  }
+
+  private String getUpdateType(VersionObj remoteVersion, boolean ignoreSuffix) {
+    final boolean isMajorSame = clientVersion.getMajor() == remoteVersion.getMajor();
+    final boolean isMinorSame = clientVersion.getMinor() == remoteVersion.getMinor();
+    final boolean isPatchSame = clientVersion.getPatch() == remoteVersion.getPatch();
+    final boolean isSuffixSame =
+      ignoreSuffix || clientVersion.getSuffix().equals(remoteVersion.getSuffix());
+    if (isMajorSame && isMinorSame && isPatchSame && isSuffixSame) {
+      return "latest";
+    } else if (isMajorSame && isMinorSame && isPatchSame) {
+      return "suffix";
+    } else if (isMajorSame && isMinorSame) {
+      return "patch";
+    } else if (isMajorSame) {
+      return "minor";
+    } else {
+      return "major";
+    }
+  }
+
+  public String constructUpdateMessage(VersionObj remoteVersion, boolean ignoreSuffix) {
+    final String updateType = getUpdateType(remoteVersion, ignoreSuffix);
+    final String remoteVersionString = remoteVersion.asStringNotation();
+    final String clientVersionString = clientVersion.asStringNotation();
+    final String clientVersionNotice = "現在のバージョンは" + clientVersionString + "です。";
+    return switch (updateType) {
+      case "latest" -> "現在最新バージョンのソフトウェアを使用中です！最新こそ正義！";
+      case "suffix" -> "新しい試験運用版のソフトウェアが利用可能です！未知への冒険へ、出発です！: "
+        + remoteVersionString + "。 " + clientVersionNotice;
+      case "patch" -> "新しいパッチリリースのソフトウェアが利用可能です！バグ修正などが含まれています！: "
+        + remoteVersionString + "。 " + clientVersionNotice;
+      case "minor" -> "新しいマイナーリリースのソフトウェアが利用可能です！新機能や小規模な機能の挙動改善が含まれています！: "
+        + remoteVersionString + "。 " + clientVersionNotice;
+      case "major" -> "新しいメジャーリリースのソフトウェアが利用可能です！大規模な機能追加や大幅な仕様変更が含まれています！更新の際には互換性にご注意ください！: "
+        + remoteVersionString + "。 " + clientVersionNotice;
+      default ->
+        "更新の種類を特定できません。直接アップデートを確認してください。" + clientVersionNotice;
+    };
+  }
+}
---
+++ src/main/java/io/github/hizumiaoba/mctimemachine/internal/version/VersionObj.java
@@ -0,0 +1,43 @@
+package io.github.hizumiaoba.mctimemachine.internal.version;
+
+import io.github.hizumiaoba.mctimemachine.api.Suffix;
+import io.github.hizumiaoba.mctimemachine.api.Version;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class VersionObj {
+
+  private final int major;
+  private final int minor;
+  private final int patch;
+  private Suffix suffix;
+
+  public static VersionObj parse(String versionNotation) {
+    // versionNotation should be "major.minor.patch" or "major.minor.patch-suffix"
+    // and may contain "v" prefix
+    String[] versionParts = versionNotation.replace("v", "").split("-");
+    String[] versionNumbers = versionParts[0].split("\\.");
+    int major = Integer.parseInt(versionNumbers[0]);
+    int minor = Integer.parseInt(versionNumbers[1]);
+    int patch = Integer.parseInt(versionNumbers[2]);
+    Suffix suffix = versionParts.length > 1 ? Suffix.parse(versionParts[1]) : Suffix.NONE;
+    return new VersionObj(major, minor, patch, suffix);
+  }
+
+  public static VersionObj parse(Version versionAnnotation) {
+    int major = versionAnnotation.major();
+    int minor = versionAnnotation.minor();
+    int patch = versionAnnotation.patch();
+    Suffix suffix = versionAnnotation.suffix();
+    return new VersionObj(major, minor, patch, suffix);
+  }
+
+  public String asStringNotation() {
+    return String.format("%d.%d.%d-%s", major, minor, patch, suffix.getSuffix());
+  }
+}
