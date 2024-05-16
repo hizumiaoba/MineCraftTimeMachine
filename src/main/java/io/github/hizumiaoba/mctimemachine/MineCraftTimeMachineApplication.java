97c4cf1c6a819b76c1bc7b7ad6bc1dcc036cb378
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-05-16 19:30:26 +0900

    Revert "chore: increment version to v0.3.0-alpha"
    This reverts commit 1659a7b946d5cb0f9b26626331c9e76534c0a744.

--- src/main/java/io/github/hizumiaoba/mctimemachine/MineCraftTimeMachineApplication.java
+++ src/main/java/io/github/hizumiaoba/mctimemachine/MineCraftTimeMachineApplication.java
@@ -12,7 +12,7 @@ import javafx.stage.WindowEvent;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Version(major = 0, minor = 3, patch = 0, suffix = Suffix.ALPHA)
+@Version(major = 0, minor = 2, patch = 0, suffix = Suffix.ALPHA)
 public class MineCraftTimeMachineApplication extends Application {
 
   @Override
