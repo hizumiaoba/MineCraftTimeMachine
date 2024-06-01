ab4f3367b5350eecccc90f5e07079e1b4dd2329e
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-06-01 12:31:29 +0900

    add GUI components for backup list viewer

---
+++ src/main/java/io/github/hizumiaoba/mctimemachine/BackupManagerController.java
@@ -0,0 +1,143 @@
+package io.github.hizumiaoba.mctimemachine;
+
+import io.github.hizumiaoba.mctimemachine.api.ExceptionPopup;
+import io.github.hizumiaoba.mctimemachine.internal.fs.BackupUtils;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BackupManagerController {
+
+  @FXML
+  private ListView<String> backupFolderListView;
+
+  @FXML
+  private TextField textFieldBackupFolderName;
+
+  @FXML
+  private Label labelBackupDateCreated;
+
+  @FXML
+  private Label labelBackupKind;
+
+  @FXML
+  private Label labelBackupDataSize;
+
+  @FXML
+  private Label labelCountBackedupWorlds;
+
+  @Setter
+  private BackupUtils backupUtils;
+
+  @FXML
+  private void initialize() {
+    log.info("BackupManagerController initialized.");
+    ObservableList<String> items = FXCollections.observableArrayList();
+    try {
+      items = FXCollections.observableList(
+        this.backupUtils.getBackupDirPaths().parallelStream().map(p -> p.getFileName().toString())
+          .toList());
+    } catch (IOException e) {
+      log.error("Failed to get backup directory names", e);
+      ExceptionPopup p = new ExceptionPopup(e, "バックアップフォルダ名の取得に失敗しました",
+        "BackupManagerController#initialize");
+      p.pop();
+    } finally {
+      this.backupFolderListView.setItems(items);
+    }
+  }
+
+  @FXML
+  private void onDeleteButtonClicked(ActionEvent e) {
+    List<Path> dirList;
+    try {
+      dirList = this.backupUtils.getBackupDirPaths();
+    } catch (IOException ex) {
+      log.error("Failed to traverse backup directories", ex);
+      ExceptionPopup p = new ExceptionPopup(ex, "バックアップフォルダの走査に失敗しました",
+        "BackupManagerController#onDeleteButtonClicked");
+      p.pop();
+      return;
+    }
+    dirList
+      .parallelStream()
+      .filter(d -> d.getFileName()
+        .toString()
+        .equals(
+          this.backupFolderListView.getSelectionModel()
+            .getSelectedItem()))
+      .findFirst()
+      .ifPresentOrElse(d -> {
+        try {
+          this.backupUtils.deleteBackupRecursively(d);
+        } catch (IOException ex) {
+          log.error("Failed to delete backup", ex);
+          ExceptionPopup p = new ExceptionPopup(ex, "バックアップの削除に失敗しました",
+            "BackupManagerController#onDeleteButtonClicked$lambda-1");
+          p.pop();
+        }
+      }, this::onErrorFindingDir);
+  }
+
+  @FXML
+  private void onCopyDirectoryButtonClicked(ActionEvent e) {
+    List<Path> dirList;
+    try {
+      dirList = this.backupUtils.getBackupDirPaths();
+    } catch (IOException ex) {
+      log.error("Failed to traverse backup directories", ex);
+      ExceptionPopup p = new ExceptionPopup(ex, "バックアップフォルダの走査に失敗しました",
+        "BackupManagerController#onCopyDirectoryButtonClicked");
+      p.pop();
+      return;
+    }
+    dirList
+      .parallelStream()
+      .filter(d -> d.getFileName()
+        .toString()
+        .equals(
+          this.backupFolderListView.getSelectionModel()
+            .getSelectedItem()))
+      .findFirst()
+      .ifPresentOrElse(d -> {
+        try {
+          this.backupUtils.duplicate(d);
+        } catch (IOException ex) {
+          log.error("Failed to copy backup", ex);
+          ExceptionPopup p = new ExceptionPopup(ex, "バックアップのコピーに失敗しました",
+            "BackupManagerController#onCopyDirectoryButtonClicked$lambda-1");
+          p.pop();
+        }
+      }, this::onErrorFindingDir);
+  }
+
+  @FXML
+  private void onCloseButtonClicked(ActionEvent e) {
+    log.info("Close button clicked.");
+    Stage s = (Stage) this.backupFolderListView.getScene().getWindow();
+    s.close();
+  }
+
+  private void onErrorFindingDir() {
+    log.warn(
+      "No backup folder found. possibly no folder selected or encountered an error internally");
+    log.trace("Got selected item: {}",
+      this.backupFolderListView.getSelectionModel().getSelectedItem());
+    log.trace("Got backup folder list: {}", this.backupFolderListView.getItems());
+    JOptionPane.showMessageDialog(null,
+      "バックアップフォルダが見つかりませんでした。選択されていないか、内部でエラーが発生している可能性があります。",
+      "エラー", JOptionPane.ERROR_MESSAGE);
+  }
+}
