feef310c3f4341ee0a5aa5fa69d9c9c700bff661
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-08-06 10:54:47 +0900

    Delete Version check logic (#91)
    * add sasl module
    
    * add okhttp dependency for plugging original connector
    
    * downgrade okhttp deps for not using alpha version
    
    * Add method to create Unsafe client for fixing #89
    
    * Change sentence for more explanation
    
    * Delete Version check functionality due that no workaround was found
    
    * Delete `VersionHelper` and related tests for vulnerable

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.github.hizumiaoba.mctimemachine {
   requires javafx.fxml;
   requires kotlin.stdlib;
   requires java.desktop;
+  requires java.security.sasl;
   requires org.xerial.sqlitejdbc;
   requires org.slf4j;
   requires ch.qos.logback.classic;
@@ -11,6 +12,8 @@ module io.github.hizumiaoba.mctimemachine {
   requires annotations;
   requires jintellitype;
   requires org.kohsuke.github.api;
+  requires org.apache.commons.lang3;
+  requires okhttp3;
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
 
