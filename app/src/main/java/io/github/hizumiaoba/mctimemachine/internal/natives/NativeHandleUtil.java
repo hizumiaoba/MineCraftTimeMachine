d7dc5c79b03f7d13ea0fcbaf1e9e55a7a4696d19
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-08-30 14:21:28 +0900

    Add Native process id handle method

--- app/src/main/java/io/github/hizumiaoba/mctimemachine/internal/natives/NativeHandleUtil.java
+++ app/src/main/java/io/github/hizumiaoba/mctimemachine/internal/natives/NativeHandleUtil.java
@@ -1,8 +1,10 @@
 package io.github.hizumiaoba.mctimemachine.internal.natives;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -13,15 +15,23 @@ public class NativeHandleUtil {
   private NativeHandleUtil() {}
 
   public static Optional<ProcessHandle> getMinecraftProcessId() {
-    ProcessBuilder pwsh = new ProcessBuilder("powershell", "-Command", "Get-Process -Name MinecraftLauncher | Where-Object { $_.MainWindowTitle -like \"Minecraft*\" } | Select-Object -ExpandProperty Id");
-    BufferedReader reader = null;
+    ProcessBuilder pwsh = new ProcessBuilder("powershell", "-Command", "\"Get-Process -Name javaw | Where-Object { $_.MainWindowTitle -like 'Minecraft*' } | Select-Object -ExpandProperty Id | Out-File -Encoding ASCII .\\.mctm-mc.id.txt \"");
+    final String idFile = ".mctm-mc.id.txt";
+    List<String> lines;
     try {
-      reader = new BufferedReader(new InputStreamReader(pwsh.start().getInputStream()));
+      Process p = pwsh.start();
+      p.onExit().join();
+      Path path = Paths.get(idFile);
+      if (Files.notExists(path)) {
+        log.warn("File {} cannot be found", idFile);
+        return Optional.empty();
+      }
+      lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+      Files.deleteIfExists(path);
     } catch (IOException e) {
       log.error("Failed to start shell process to find Minecraft process id", e);
       return Optional.empty();
     }
-    List<String> lines = reader.lines().toList();
     if(lines.size() != 1) {
       log.warn("It seems that there are multiple Minecraft processes running? Found {} processes: ", lines.size());
       lines.forEach(log::warn);
@@ -29,6 +39,14 @@ public class NativeHandleUtil {
       return Optional.empty();
     }
     log.trace("Found Minecraft process id: {}", lines.get(0));
-    return ProcessHandle.of(Long.parseLong(lines.get(0)));
+    int pid = Integer.parseInt(lines.get(0).trim());
+    return ProcessHandle.of(pid);
   }
+
+  @Deprecated
+  public static Optional<ProcessHandle> getMinecraftProcess() {
+    Optional<ProcessHandle> handle = ProcessHandle.allProcesses().parallel().filter(h -> h.info().command().isPresent()).filter(h -> h.info().command().get().contains("javaw")).findFirst();
+    return handle;
+  }
+
 }
