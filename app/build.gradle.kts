e46515534c3a5307e143685c5823ade5e62977f7
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-10-03 18:22:55 +0900

    build: add installer related options for ease installing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ jlink {
     }
     jpackage {
         imageOptions.addAll(listOf("--icon", "${projectDir}/src/main/resources/assets/icon.ico"))
+        installerOptions.addAll(listOf("--win-per-user-install","--win-dir-chooser", "--win-menu", "--win-shortcut", "--win-shortcut-prompt"))
     }
 }
 
