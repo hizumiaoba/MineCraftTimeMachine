fcda0550126ab09a7bd41aa919a3ef3e7514c5d9
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-12-20 22:49:20 +0900

    fix data class converting

--- app/src/main/java/io/github/hizumiaoba/mctimemachine/api/version/MinimalRemoteVersionCrate.java
+++ app/src/main/java/io/github/hizumiaoba/mctimemachine/api/version/MinimalRemoteVersionCrate.java
@@ -2,10 +2,13 @@ package io.github.hizumiaoba.mctimemachine.api.version;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.IOException;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.kohsuke.github.GHAsset;
+import org.kohsuke.github.GHRelease;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -30,5 +33,27 @@ public class MinimalRemoteVersionCrate {
     private long size;
     private String contentType;
     private String name;
+
+    static AssetsCrate of(GHAsset raw) {
+      return new AssetsCrate(
+        raw.getBrowserDownloadUrl(),
+        raw.getSize(),
+        raw.getContentType(),
+        raw.getName()
+      );
+    }
+  }
+
+  public static MinimalRemoteVersionCrate of(GHRelease raw) {
+    try {
+      return new MinimalRemoteVersionCrate(
+        raw.getTagName(),
+        raw.getHtmlUrl().toExternalForm(),
+        raw.isPrerelease(),
+        List.of(raw.listAssets().toList().parallelStream().map(AssetsCrate::of).toArray(AssetsCrate[]::new))
+      );
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- app/src/test/java/io/github/hizumiaoba/mctimemachine/VersionCheckTest.java
+++ app/src/test/java/io/github/hizumiaoba/mctimemachine/VersionCheckTest.java
@@ -1,9 +1,11 @@
 package io.github.hizumiaoba.mctimemachine;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import io.github.hizumiaoba.mctimemachine.api.version.VersionChecker;
 import io.github.hizumiaoba.mctimemachine.internal.version.VersionObj;
+import java.io.IOException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +13,12 @@ public class VersionCheckTest {
 
   @Test
   void checkClientConnection() {
-    VersionChecker checker = new VersionChecker();
+    VersionChecker checker = null;
+    try {
+      checker = new VersionChecker();
+    } catch (IOException e) {
+      fail(e);
+    }
     VersionObj mockClientVersion = VersionObj.parse("v1.0.0");
     checker.getLatestVersion(false)
       .ifPresentOrElse(r -> {
