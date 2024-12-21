14464e78192474a3ecec6b7d7354da794d4cef99
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-12-21 10:33:25 +0900

    fix tests

--- app/src/test/java/io/github/hizumiaoba/mctimemachine/GitHubApiTest.java
+++ app/src/test/java/io/github/hizumiaoba/mctimemachine/GitHubApiTest.java
@@ -1,37 +1,44 @@
 package io.github.hizumiaoba.mctimemachine;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jcabi.github.Github;
-import com.jcabi.github.RtGithub;
-import com.jcabi.http.response.JsonResponse;
 import java.io.IOException;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GHRelease;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 @Slf4j
 @Disabled("This test requires a GitHub API token.")
 public class GitHubApiTest {
-  ObjectMapper mapper = new ObjectMapper();
+
+  public static final String REPO_FULL_NAME = "hizumiaoba/MineCraftTimeMachine";
 
   @Test
-  void repos() throws IOException {
-    Github github = new RtGithub();
-    final JsonResponse response = github.entry()
-      .uri().path("/repos/hizumiaoba/MineCraftTimeMachine/releases")
-      .back()
-      .fetch()
-      .as(JsonResponse.class);
-    JsonNode json = mapper.readTree(response.body());
-    log.trace("raw json response:");
-    log.trace(mapper.writeValueAsString(json));
-    while (json.elements().hasNext()) {
-      JsonNode node = json.elements().next();
-      assertThat(node.get("tag_name").asText()).contains("v");
-      assertThat(node.get("html_url").asText()).startsWith("https://github.com/");
+  void repos() {
+    GitHub github;
+    try {
+      github = GitHub.connectAnonymously();
+    } catch (IOException e) {
+      fail(e);
+      return;
     }
+    GHRepository repo;
+    List<GHRelease> releases;
+    try {
+      repo = github.getRepository(REPO_FULL_NAME);
+      releases = repo.listReleases().toList();
+    } catch (IOException e) {
+      fail(e);
+      return;
+    }
+
+    assertThat(repo).isNotNull();
+    assertThat(repo.getFullName()).isEqualTo(REPO_FULL_NAME);
+    assertThat(releases).isNotEmpty();
   }
 }
